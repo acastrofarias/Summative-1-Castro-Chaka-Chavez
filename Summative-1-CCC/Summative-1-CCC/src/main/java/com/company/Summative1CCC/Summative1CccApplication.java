7b043d8ee933379b64d0d6e3f0f6b0a4fc93e6e0
Adrian Castro <acastrofarias13@gmail.com>
2023-06-29 16:06:56 -0700

    adding starter files

---
+++ Summative-1-CCC/Summative-1-CCC/src/main/java/com/company/Summative1CCC/Summative1CccApplication.java
@@ -0,0 +1,13 @@
+package com.company.Summative1CCC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Summative1CccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Summative1CccApplication.class, args);
+	}
+
+}
