09bda7870609b3aad05d7645417033de00337df6
Adrian Castro <acastrofarias13@gmail.com>
2023-06-29 16:12:44 -0700

    reuploading starter folder

---
+++ Summative-1-CCC/src/test/java/com/company/Summative1CCC/Summative1CccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.Summative1CCC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Summative1CccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
