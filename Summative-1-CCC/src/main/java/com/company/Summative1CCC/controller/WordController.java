f8972b51b48925d4917e3e54825da1133bb75df3
crystalchavez99 <chavezcrystal1999@yahoo.com>
2023-06-29 16:40:23 -0700

    added file stucture for word api

---
+++ Summative-1-CCC/src/main/java/com/company/Summative1CCC/controller/WordController.java
@@ -0,0 +1,4 @@
+package com.company.Summative1CCC.controller;
+
+public class WordController {
+}
--- Summative-1-CCC/src/main/java/com/company/Summative1CCC/models/Answer.java
+++ Summative-1-CCC/src/main/java/com/company/Summative1CCC/models/Answer.java
@@ -1,5 +1,7 @@
 package com.company.Summative1CCC.models;
 
 public class Answer {
-
+    public int id;
+    public String question;
+    public String answer;
 }
