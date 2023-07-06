fa32cd48906731666755f02b7f7cb90f43b21871
crystalchavez99 <chavezcrystal1999@yahoo.com>
2023-07-06 15:09:45 -0700

    definition model and controller

--- Summative-1-CCC/src/main/java/com/company/Summative1CCC/controller/WordController.java
+++ Summative-1-CCC/src/main/java/com/company/Summative1CCC/controller/WordController.java
@@ -1,4 +1,34 @@
 package com.company.Summative1CCC.controller;
 
+import com.company.Summative1CCC.models.Definition;
+
+import org.springframework.web.bind.annotation.RestController;
+import com.company.Summative1CCC.models.Answer;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+
 public class WordController {
+    private List<Definition> wordList;
+
+    private static int idCounter = 1;
+
+    public WordController(){
+        wordList.add(new Definition("Cosher", "To treat with special kindess", idCounter++));
+        wordList.add(new Definition(("Logophile", "A lover of words", idCounter++)));
+        wordList.add(new Definition("Ferhoodle", "To confuse or mix up", idCounter++));
+        wordList.add(new Definition("Proceleusmatic", "Inciting or inspiring", idCounter++));
+        wordList.add(new Definition("Skrik", "A sudden fright", idCounter++));
+        wordList.add(new Definition("Anamnesis", "Remembrance of the past", idCounter++));
+        wordList.add(new Definition("Gazetteer", "Geographical dictionary", idCounter++));
+        wordList.add(new Definition("Mauka", "Towards the mountains", idCounter++));
+        wordList.add(new Definition("Pervicacious", "Extremely willful", idCounter++));
+        wordList.add(new Definition("Artiodactly", "Even number of toes on each foot", idCounter++));
+        
+    }
+
+
 }
--- Summative-1-CCC/src/main/java/com/company/Summative1CCC/models/Definition.java
+++ Summative-1-CCC/src/main/java/com/company/Summative1CCC/models/Definition.java
@@ -6,4 +6,35 @@ public class Definition {
     public String word;
 
     public String definition;
+
+    public Definition(){};
+    public Definition(String word, String definition, int id){
+        this.word = word;
+        this.definition = definition;
+        this.id = id;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getWord(){
+        return word;
+    }
+
+    public void setWord(String word){
+        this.word = word;
+    }
+
+    public String getDefinition(){
+        return definition;
+    }
+
+    public void setDefinition(String definition){
+        this.definition = definition;
+    }
 }
