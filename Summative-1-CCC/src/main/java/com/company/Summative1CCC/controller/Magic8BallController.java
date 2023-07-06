b8da81ee37db5ec969d97793bb226e70930a4d57
Adrian Castro <acastrofarias13@gmail.com>
2023-07-06 14:48:25 -0700

    started Magic8BallController

--- Summative-1-CCC/src/main/java/com/company/Summative1CCC/controller/Magic8BallController.java
+++ Summative-1-CCC/src/main/java/com/company/Summative1CCC/controller/Magic8BallController.java
@@ -1,5 +1,20 @@
 package com.company.Summative1CCC.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+import com.company.Summative1CCC.models.Answer;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class Magic8BallController {
 
+    private List<Answer> answerList;
+
+    public Magic8BallController(){
+        
+    }
 }
